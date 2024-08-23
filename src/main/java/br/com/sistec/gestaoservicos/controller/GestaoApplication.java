7083aba2bdb1c13aff315a7e0ad89fa238b7bccb
Vinicius Barbosa <vinicius07ab@gmail.com>
2024-08-23 13:42:55 -0300

    alteração do projeto

--- src/main/java/br/com/sistec/gestaoservicos/controller/GestaoApplication.java
+++
@@ -1,7 +0,0 @@
-package br.com.sistec.gestaoservicos.controller;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class GestaoApplication {
-}
